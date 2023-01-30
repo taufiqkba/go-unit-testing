a1380107ff51ade4091029d149732a036220701f
Taufiq Kurniawan <taufiqkurniawan9991@gmail.com>
2023-01-30 22:08:43 +0700

    Learning to fail a unit test

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -1,19 +1,28 @@
 package helper
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestHelloWorld(t *testing.T) {
-	result := HelloWorld("Budi")
+	result := HelloWorld("Taufiq")
 	if result != "Hello Taufiq" {
 		// unit test failed
-		panic("Result is no Hello Taufiq")
+		// t.Fail() // Still continue to running another code
+		t.Error("Result must be 'Hello Taufiq'")
 	}
+
+	fmt.Println("Test DONE!")
 }
 
 func TestHelloWorldKurniawan(t *testing.T) {
 	result := HelloWorld("Kurniawan")
 	if result != "Hello Kurniawan" {
 		// unit test failed
-		panic("Result is no Hello Kurniawan")
+		// t.FailNow() // Code stop running with FailNow()
+		t.Fatal("Result must be 'Hello Kurniawan'")
 	}
+
+	fmt.Println("Test DONE 2")
 }
